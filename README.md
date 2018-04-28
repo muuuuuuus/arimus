5d89a6311650fb42fff1f4d6cad01eaad8ea1c70
Mus <38335722+muuuuuuus@users.noreply.github.com>
2018-04-29 01:06:41 +0200

    add initial, simple vaadin page

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # arimus
 a project for playing around and have fun
+
+# to try out the application, run 
+mvn jetty:run 
+and open localhost:8080
--- src/main/java/de/arimus/Application.java
+++
@@ -1,9 +0,0 @@
-package de.arimus;
-
-public class Application {
-
-	public static void main(String[] args) {
-		System.out.println("an apple a day keeps the doctor away");
-	}
-
-}
